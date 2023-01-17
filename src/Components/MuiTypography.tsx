4fddea11d2bd96d43d87b06d5cedc78bfc3da9da
George Kimari <grgkimari@gmail.com>
2023-01-17 23:44:23 +0300

    Typography

--- src/Components/MuiTypography.tsx
+++ src/Components/MuiTypography.tsx
@@ -1,8 +1,20 @@
-
+import { Typography } from "@mui/material"
 
 const MuiTypography = () => {
     return(
-        <div>Mui Typography</div>
+        <div>
+            <Typography variant="h1">h1 Typography</Typography>
+            <Typography variant="h2">h2 Typography</Typography>
+            <Typography variant="h3">h3 Typography</Typography>
+            <Typography variant="h4" gutterBottom>h4 Typography</Typography>
+            <Typography variant="h5">h5 Typography</Typography>
+            <Typography variant="h6">h6 Typography</Typography>
+            <Typography variant="subtitle1">subtitle1 Typography</Typography>
+            <Typography variant="subtitle2">subtitle2 Typography</Typography>
+            <Typography variant="body1">body1 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat inventore error eum eius, exercitationem rem sed porro accusamus? Nostrum ullam ratione eos beatae quod, blanditiis aliquam reiciendis hic iusto doloribus! Typography</Typography>
+            <Typography variant="body2">body2 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, modi, consequuntur libero minus repudiandae, quam exercitationem laboriosam repellendus in totam perferendis autem dolore! Esse eius harum quasi possimus unde et? Typography</Typography>
+
+        </div>
     )
 }
 
